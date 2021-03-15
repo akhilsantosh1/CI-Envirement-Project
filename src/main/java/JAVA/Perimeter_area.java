f99a24186f2d4a1e9785ce9df85291fa5e00612e
raashita99 <55356206+raashita99@users.noreply.github.com>
2021-03-15 20:20:22 +0100

    final tests

--- src/main/java/JAVA/Perimeter_area.java
+++ src/main/java/JAVA/Perimeter_area.java
@@ -3,22 +3,22 @@ package JAVA;
 import java.util.Scanner;
 
 public class Perimeter_area {
-	static int Square_area(int l)  
+	static int Square_area(int l)  //calculates and returns the area of Square
 	{
 		return(l*l);
 	}
 	
-	static int Square_perimeter(int l) 
+	static int Square_perimeter(int l) //calculates and returns the Perimeter of Square
 	{
 		return(4*l);
 	}
 	
-	static int rectangle_perimeter(int l,int b)
+	static int rectangle_perimeter(int l,int b) //calculates and returns the perimeter of rectangle 
 	{
 		return(2*(l+b));
 	}
 	
-	static int rectangle_area(int l,int b)
+	static int rectangle_area(int l,int b)   //calculates and returns the area of rectangle 
 	{
 		return(l*b);
 	}
@@ -26,16 +26,16 @@ public class Perimeter_area {
 	public static void main(String[] args) {
 	int l,b;
 	Scanner s = new Scanner(System.in);
-	System.out.println("Enter the length of the square");
+	System.out.println("Enter the length of the square");  // Manual input for length of square 
 	l =s.nextInt();
-	System.out.println("The area and perimeter of square is:");
+	System.out.println("The area and perimeter of square is:"); // area and perimeter of square result 
 	System.out.println(Square_area(l));
 	System.out.println(Square_perimeter(l));
-	System.out.println("Enter the length of the rectangle");
+	System.out.println("Enter the length of the rectangle");  //manual input for length of rectangle 
 	l =s.nextInt();
-	System.out.println("Enter the breadth of the rectangle");
+	System.out.println("Enter the breadth of the rectangle");  //manual input for breadth of rectangle 
 	b =s.nextInt();
-	System.out.println("The area and perimeter of rectangle is:");
+	System.out.println("The area and perimeter of rectangle is:");  // area and perimeter of the rectangle result 
 	System.out.println(rectangle_area(l,b));
 	System.out.println(rectangle_perimeter(l,b));
 }
