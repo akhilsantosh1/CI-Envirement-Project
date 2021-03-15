d6d7fdce16e679d900e0fa443d618086b8e3816d
Akhisantosh1234 <akhilsantosht@gmail.com>
2021-03-15 05:48:14 +0100

    test changes

---
+++ src/test/java/JAVA/Java_tests.java
@@ -0,0 +1,34 @@
+package JAVA;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Java_tests {
+	Perimeter_area junit= new Perimeter_area(); 
+
+	@Test
+	public void rectangle_area_test() {
+			int result= Perimeter_area.rectangle_area(3,4);
+			assertEquals (12,result);
+	}
+	
+	@Test
+	public void rectangle_perimetertest() { 
+		int result= Perimeter_area.rectangle_perimeter(2,3);
+		assertEquals (10,result);
+	}
+		
+	@Test
+	public void square_area_test() { 
+		int result= Perimeter_area.Square_area(2);
+		assertEquals (4,result);
+	}
+	
+	@Test
+	public void square_perimeter_test() {
+		int result= Perimeter_area.Square_perimeter(4);
+		assertEquals (16,result);
+	}
+}
+
