4e2406f903b842e8143cb8bd8096c5733f99c48f
Akhisantosh1234 <akhilsantosht@gmail.com>
2021-03-15 21:56:07 +0100

    main code

---
+++ src/main/java/JAVA/Perimeter_area.java
@@ -0,0 +1,42 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class Perimeter_area {
+	static int Square_area(int l)  //calculates and returns the area of Square
+	{
+		return(l*l);
+	}
+	
+	static int Square_perimeter(int l) //calculates and returns the Perimeter of Square
+	{
+		return(4*l);
+	}
+	
+	static int rectangle_perimeter(int l,int b) //calculates and returns the perimeter of rectangle 
+	{
+		return(2*(l+b));
+	}
+	
+	static int rectangle_area(int l,int b)   //calculates and returns the area of rectangle 
+	{
+		return(l*b);
+	}
+
+	public static void main(String[] args) {
+	int l,b;
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the length of the square");  // Manual input for length of square 
+	l =s.nextInt();
+	System.out.println("The area and perimeter of square is:"); // area and perimeter of square result 
+	System.out.println(Square_area(l));
+	System.out.println(Square_perimeter(l));
+	System.out.println("Enter the length of the rectangle");  //manual input for length of rectangle 
+	l =s.nextInt();
+	System.out.println("Enter the breadth of the rectangle");  //manual input for breadth of rectangle 
+	b =s.nextInt();
+	System.out.println("The area and perimeter of rectangle is:");  // area and perimeter of the rectangle result 
+	System.out.println(rectangle_area(l,b));
+	System.out.println(rectangle_perimeter(l,b));
+}
+}
