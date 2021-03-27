81f8600f5af159b94b2d63d5c8c5b51cf0cddb03
Akhisantosh1234 <akhilsantosht@gmail.com>
2021-03-27 21:35:50 +0100

    commit change

--- src/main/java/JAVA/Perimeter_area.java
+++ src/main/java/JAVA/Perimeter_area.java
@@ -28,7 +28,7 @@ public class Perimeter_area {
 	Scanner s = new Scanner(System.in);
 	System.out.println("Enter the length of the square");  // Manual input for length of square 
 	l =s.nextInt();
-	System.out.println("The area and perimeter of square:"); // area and perimeter of square result 
+	System.out.println("The area and perimeter of square is:"); // area and perimeter of square result 
 	System.out.println(Square_area(l));
 	System.out.println(Square_perimeter(l));
 	System.out.println("Enter the length of the rectangle");  //manual input for length of rectangle 
@@ -38,6 +38,5 @@ public class Perimeter_area {
 	System.out.println("The area and perimeter of rectangle is:");  // area and perimeter of the rectangle result 
 	System.out.println(rectangle_area(l,b));
 	System.out.println(rectangle_perimeter(l,b));
-	System.out.println("test akhil");
 }
 }
